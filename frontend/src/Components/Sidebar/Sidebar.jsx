793b2a0d87722d087f88150823de6b7de1f1fc18
= <=>
2024-06-03 18:45:40 +0530

    finishing form validation

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -1,23 +1,25 @@
 import React from 'react'; 
-import { Link } from 'react-router-dom';
+import { NavLink as Link } from 'react-router-dom';
 
 
 const Sidebar = () => {
+ 
+
   return (
     <div className="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
       <div className="sidebar-brand">
-        <Link href="index.html"> <img alt="logo" src="assets/img/logo.png" className="header-logo" /> <span
+        <Link to="/"> <img alt="logo" src="assets/img/logo.png" className="header-logo" /> <span
             className="logo-name">Otika</span>
         </Link>
       </div>
       <ul className="sidebar-menu">
         <li className="menu-header">Main</li>
-        <li className="dropdown active">
+        <li className={`dropdown`}>
           <Link to="/" className="nav-link"><i className='fas fa-fire'></i><span>Dashboard</span></Link>
         </li>
 
-        <li className="dropdown">
+        <li className={`dropdown`}>
           <Link to="/add-user" className="nav-link"><i className='fas fa-user-plus'></i><span>Add User</span></Link>
         </li>
         
