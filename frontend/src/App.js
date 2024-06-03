b535c43b0c9db7fff41e62cb0744c847ac800303
= <=>
2024-06-03 15:33:48 +0530

    adding router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-        
+       <div class="main-wrapper main-wrapper-1">
+         <div class="navbar-bg"></div>  
+          <Router>
+            
+            <Routes>
+
+              
+            </Routes>
+          </Router>
+       </div>
     </div>
   );
 }
