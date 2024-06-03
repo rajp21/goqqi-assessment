2dc34140f74598b8f0e8e135c778e1daf6e0e549
= <=>
2024-06-03 19:49:39 +0530

    controller setup completed

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -7,7 +7,7 @@ const apiRouter = express.Router();
 apiRouter.post('/add-user', userController.addUser);
 apiRouter.get('/get-users', userController.getUsers); 
 apiRouter.put('/update-user', userController.updateUser); 
-apiRouter.delete('/delete-user', userController.deleteUser);
+apiRouter.delete('/delete-user', userController.deleteUser); 
 
 
-export default apiRouter; 
+export default apiRouter;
