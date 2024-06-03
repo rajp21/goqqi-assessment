12d7fcddb386e4dbb7f5df33ec1d845829d50ddb
= <=>
2024-06-04 01:43:39 +0530

    updation and deletion api completed

--- backend/src/controllers/user/userController.js
+++ backend/src/controllers/user/userController.js
@@ -4,8 +4,7 @@ import CustomErrorHandler from "../../services/CustomErrorHandler";
 
 const userController = { 
     async addUser(req, res, next){ 
-        
-
+        // validating the request
         const userSchema = Joi.object({ 
             name: Joi.string().min(2)
             .max(50)
@@ -41,7 +40,7 @@ const userController = {
 
             dob: Joi.date()
             .iso()
-            .max('now') // Maximum date is today's date
+            .max('now') 
             .required()
             .messages({
               'date.base': 'Date of birth must be a valid date',
@@ -56,6 +55,7 @@ const userController = {
       
         if(error) return next(error); 
 
+        // checking weather user already exist or not
         let exist; 
 
         try{ 
@@ -69,32 +69,146 @@ const userController = {
             return next(CustomErrorHandler.userAlreadyExist(422, "User Already Exist")); 
         }
         
+
+        // storing the user in the db
         let user; 
         try {
-            user = await User.create(data); 
-
+            user = await User.create(req.body); 
         }catch(error){ 
-            console.log('error is here'); 
+            return next(error); 
         }
-
+        
 
         return res.status(200).json({
-            success: true
-        })
-
-
+            success: true, 
+            user: user
+        }); 
     }, 
 
     async getUsers(req, res, next){ 
+        let users; 
+        try{ 
+            users = await User.findAll(); 
+        }catch(error){ 
+            return next(error); 
+        }
 
+        return res.status(200).json({
+            success: true, 
+            users
+        }); 
     }, 
 
     async updateUser(req, res, next){ 
+        const userSchema = Joi.object({ 
+            userId: Joi.number()
+            .integer() 
+            .positive() 
+            .required()
+            .messages({
+              'number.base': 'User ID must be a number',
+              'number.integer': 'User ID must be an integer',
+              'number.positive': 'User ID must be a positive number',
+              'any.required': 'User ID is required'
+            }),
+
+            name: Joi.string().min(2)
+            .max(50)
+            .pattern(/^[A-Za-z ]*$/)
+            .required()
+            .messages({
+              'string.base': 'Name must be a string',
+              'string.empty': 'Name is required',
+              'string.min': 'Name must be at least {#limit} characters long',
+              'string.max': 'Name must be at most {#limit} characters long',
+              'string.pattern.base': 'Name must only contain alphabets and spaces'
+            }), 
+            
+            email: Joi.string()
+            .email({tlds: {allow: false}})
+            .required()
+            .messages({ 
+                'string.base': 'Email must be a string',
+                'string.empty': 'Email is required',
+                'string.email': 'Email must be a valid email address'
+            }), 
+            
+            password: Joi.string()
+            .min(6) 
+            .max(30)
+            .required()
+            .messages({
+              'string.base': 'Password must be a string',
+              'string.empty': 'Password is required',
+              'string.min': 'Password must be at least {#limit} characters long',
+              'string.max': 'Password must be at most {#limit} characters long'
+            }), 
+
+            dob: Joi.date()
+            .iso()
+            .max('now') 
+            .required()
+            .messages({
+              'date.base': 'Date of birth must be a valid date',
+              'date.empty': 'Date of birth is required',
+              'date.format': 'Date of birth must be in ISO 8601 format (YYYY-MM-DD)',
+              'date.max': 'Date of birth cannot be in the future'
+            })         
+        }); 
+
+
+        const {error} = userSchema.validate(req.body); 
+
+        if(error) return next(error); 
+
+        let updatedUer; 
+
+        try{ 
+            updatedUer = User.update(req.body, {where: {id: req.body.userId}}); 
+        }catch(error){ 
+            return next(error); 
+        }
+
+
+
+        return res.status(200).json({
+            success: true, 
+            message: "User has been successfully updated"
+        })
 
     }, 
 
     async deleteUser(req, res, next){ 
+        const schema = Joi.object({ 
+            userId: Joi.number()
+            .integer() 
+            .positive() 
+            .required()
+            .messages({
+              'number.base': 'User ID must be a number',
+              'number.integer': 'User ID must be an integer',
+              'number.positive': 'User ID must be a positive number',
+              'any.required': 'User ID is required'
+            })
+        }); 
 
+
+        const {error} = schema.validate(req.body); 
+
+        if(error) return next(error); 
+
+        let rowsAffected; 
+
+        try{    
+           rowsAffected = await User.destroy({ where: { id: req.body.userId } }); 
+        }catch(error) {
+            return next(error); 
+        }
+
+        return res.status(200).json({
+            success: true, 
+            message: "user deleted successfully"
+        });     
     }, 
 }
 
